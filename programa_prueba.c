6347db23654267366d267de816f863b0eaa8adc4
Miguel Higueras <mhiguerasp@gmail.com>
2024-06-25 20:38:34 +0200

    adivina un numero

--- programa_prueba.c
+++ programa_prueba.c
@@ -1,14 +1,39 @@
-#include <stdio.h>
+#include <stdio.h> //libreria printf scanf
+#include <stdlib.h> //libreria funcion random
 
 int main(void)
 {
     char nombre;
+    int intentos;
+    int numero_random;
+    int a;
 
-    printf("¿Como te llamas?\n");
-    //%d número base 10, %i enteros, %s string 
-    // Recoger por pantalla se hace scanf
+
+    printf("Como te llamas?\n");
+    //%d número base 10, %i enteros, %s string. Para recoger por pantalla se hace scanf
     scanf("%s", &nombre);
     printf("Hola %s, un placer conocerte", &nombre);
+
+    numero_random = rand() % 11;
+    intentos=0;
+
+    while(1)
+    {
+        printf("Dime un numero de 0 a 10\n");
+        scanf("%d", &a);
+        intentos++;
+        if (numero_random == a)
+        {
+            printf("%s Has acertado\n", &nombre);
+            printf("Estaba pensando en el %d)", numero_random);
+            printf("%s lo has logrado en %d intentos\n", &nombre, intentos);
+            break;
+        }
+        else
+        {
+            printf("Sigue intentado\n");
+        }
+    }
 
     return(0);
 }
